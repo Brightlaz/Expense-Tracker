31f8dbe3c16d0be659b603b6a6af9c503e27d742
kariebi <sorohmichael@gmail.com>
2023-10-24 19:09:22 -0400

    testing

--- client/app/(Landing Page)/page.js
+++ client/app/(Landing Page)/page.js
@@ -1,15 +1,15 @@
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 import Loader from '../components/loader';
 
-const DynamicBody = dynamic(() => import('../components/Body'), {
-  ssr: false,
-  loading: () => <div className='w-screen h-screen'><Loader /></div>
-})
+// const DynamicBody = dynamic(() => import('../components/Body'), {
+//   ssr: false,
+//   loading: () => <div className='w-screen h-screen'><Loader /></div>
+// })
 
 export default function Home() {
   return (
     <main className="">
-      <DynamicBody />
+      Testing...
     </main>
   );
 }
