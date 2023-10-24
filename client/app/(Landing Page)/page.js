9c706781404a089c45bd8e42144ec8bb227c6228
kariebi <sorohmichael@gmail.com>
2023-10-24 19:18:57 -0400

    still toying around

--- client/app/(Landing Page)/page.js
+++ client/app/(Landing Page)/page.js
@@ -1,4 +1,6 @@
 // import dynamic from 'next/dynamic'
+"use client";
+import React from "react";
 import Loader from '../components/loader';
 
 // const DynamicBody = dynamic(() => import('../components/Body'), {
