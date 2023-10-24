4885231952c33f5df7866a6eea73fbc30dd8d742
kariebi <sorohmichael@gmail.com>
2023-10-24 18:36:04 -0400

    beginning to make routes protected

---
+++ server/src/server.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const app = require('./app');
+const { initDB } = require('./config/ottoman');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+const exitHandler = () => {
+    if (server) {
+        server.close((err) => {
+            if (err) {
+                console.error('Error closing server:', err);
+                process.exit(1);
+            } else {
+                console.log('Server closed');
+                process.exit(0);
+            }
+        });
+    } else {
+        process.exit(0);
+    }
+};
+
+const startServer = async () => {
+    try {
+        await initDB();
+
+        server.listen(PORT, () => {
+            console.log(`API started at http://localhost:${PORT}`)
+            console.log(`API docs at http://localhost:${PORT}/v1/docs/`)
+        });
+
+        process.on('SIGTERM', () => {
+            console.log('SIGTERM received');
+            exitHandler();
+        });
+
+        process.on('SIGINT', () => {
+            console.log('SIGINT received');
+            exitHandler();
+        });
+
+        process.on('uncaughtException', (error) => {
+            console.log('Uncaught Exception:', error);
+            exitHandler();
+        });
+
+        process.on('unhandledRejection', (reason) => {
+            console.log('Unhandled Rejection:', reason);
+            exitHandler();
+        });
+    } catch (error) {
+        console.error('Error: App failed to start', error);
+        process.exit(1);
+    }
+};
+
+startServer();
