36a9a7e499122a47e21cca3348cf563b0dd6050c
kariebi <sorohmichael@gmail.com>
2023-10-24 20:41:10 -0400

    signing

--- client/app/SignUp/page.js
+++ client/app/SignUp/page.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Image from 'next/image'
+import Piggy from '../assets/ui/piggy.svg'
 
 const SignUp = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Image
+        src={Piggy}
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        style={{
+          width: '100%',
+          height: 'auto',
+        }}
+        width={500}
+        height={300}
+      />
+    </div>
+  )
 };
 
 export default SignUp;
