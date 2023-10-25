b1b97b46145e210da2c8304430cf7e1a3ea0a8b5
kariebi <sorohmichael@gmail.com>
2023-10-25 10:54:12 -0400

    moving to vite

---
+++ client/src/components/onboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Onboard = () => {
+  return (
+    <div>
+      <h1>Onboard page</h1>
+      <p>Onboard</p>
+    </div>
+  );
+};
+
+export default Onboard;
---
+++ client/src/components/signUp.jsx
@@ -0,0 +1,13 @@
+ 
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <p>SignIn</p>
+    </div>
+  );
+};
+
+export default SignUp;
