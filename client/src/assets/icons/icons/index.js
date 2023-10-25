3f12245dcef886ccd33a5c81e06a029194b5c5db
Bright Lazarus <lazbright1@gmail.com>
2023-10-25 16:31:59 +0100

    Update the code

---
+++ client/src/assets/icons/icons/index.js
@@ -0,0 +1,6 @@
+import Gasoline from "./Gasoline.svg";
+import groceries from "./Grocery.svg";
+import health from "./Health.svg";
+import school from "./Books.svg";
+
+export { Gasoline, groceries, health, school };
